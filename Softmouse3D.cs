6c85c08e07c99b04c8569613c4174e4e8bcab9f3
Robert Schuldt <robert.schuldt@tridicon.de>
2015-05-07 20:10:30 +0200

    Added double-click limits, and the base for button event raising.

--- Softmouse3D.cs
+++ Softmouse3D.cs
@@ -25,6 +25,7 @@ namespace HID.Softmouse3D
 				info.hid.dwVendorId==VendorID.GGS&&info.hid.dwProductId==ProductID.Softmouse3D;
 		}
 
+		#region Events
 		/// <summary>
 		/// Fired when a button is pressed down.
 		/// </summary>
@@ -54,6 +55,52 @@ namespace HID.Softmouse3D
 		/// Fired when the mouse wheel is moved.
 		/// </summary>
 		public event EventHandler<Softmouse3DEventArgs> MouseWheel;
+		#endregion
+
+		#region Limits
+		/// <summary>
+		/// Gets and sets the maximum movement, that must not exceed while clicking for
+		/// two clicks to be considered a double-click. Both directions (x and y) share
+		/// this value. Default: 5.
+		/// </summary>
+		public int MaximumDoubleClickMovement { get; set; }
+
+		/// <summary>
+		/// Gets the maximum number of milliseconds allowed between clicks for a
+		/// double-click. Default: 250 ms.
+		/// </summary>
+		public int MaximumDoubleClickTime { get; set; }
+		#endregion
+
+		/// <summary>
+		/// Creates and initializes an instance of this class.
+		/// </summary>
+		public Softmouse3D()
+		{
+			MaximumDoubleClickMovement=5;
+			MaximumDoubleClickTime=250;
+		}
+
+		enum Button
+		{
+			A1=0,
+			A2=1,
+			A3=2,
+			A4=3,
+			D1=4,
+			D2=5,
+			D3=6,
+			D4=7,
+			SL=8,
+			SR=9,
+		}
+
+		static readonly Softmouse3DButtons[] buttonFilter=new Softmouse3DButtons[] 
+		{
+			Softmouse3DButtons.A1, Softmouse3DButtons.A2, Softmouse3DButtons.A3, Softmouse3DButtons.A4,
+			Softmouse3DButtons.D1, Softmouse3DButtons.D2, Softmouse3DButtons.D3, Softmouse3DButtons.D4,
+			Softmouse3DButtons.SL, Softmouse3DButtons.SR
+		};
 
 		struct ButtonState
 		{
@@ -64,9 +111,7 @@ namespace HID.Softmouse3D
 		class State
 		{
 			public Softmouse3DButtons Buttons;
-			public ButtonState D1, D2, D3, D4;
-			public ButtonState A1, A2, A3, A4;
-			public ButtonState SL, SR;
+			public ButtonState[] ButtonsStates=new ButtonState[10];
 		}
 
 		Dictionary<IntPtr, State> StatePreDevice=new Dictionary<IntPtr, State>();
@@ -116,16 +161,93 @@ namespace HID.Softmouse3D
 						{
 							Softmouse3DEventData eventData=(Softmouse3DEventData)Marshal.PtrToStructure(buffer+inputHeaderSize+i*(int)Softmouse3DEventData.SIZE, typeof(Softmouse3DEventData));
 
-							// TODO
+							Softmouse3DButtons buttons=(Softmouse3DButtons)(eventData.ButtonState&0x000f3f00);
 
-							//last.D1.Down=ticks;
-							//last.D1.MoveX=0;
-							//last.D1.MoveY=0;
-							//last.D1.Clicks=0;
+							#region Add movement to previously pressed buttons and handle if limit a exceeded
+							for(int b=0; b<10; b++)
+							{
+								if(last.ButtonsStates[b].Clicks==0) continue;
 
-							//evt(this, new Softmouse3DEventArgs(inputHeader.header.hDevice, eventData.X, eventData.Y, eventData.Z, (Softmouse3DButtons)eventData.ButtonState));
+								// sum up movement
+								last.ButtonsStates[i].MoveX+=eventData.X;
+								last.ButtonsStates[i].MoveY+=eventData.Y;
+
+								if(Math.Abs(last.ButtonsStates[i].MoveX)>MaximumDoubleClickMovement||
+									Math.Abs(last.ButtonsStates[i].MoveY)>MaximumDoubleClickMovement||
+									(ticks-last.ButtonsStates[i].Down)/10000>MaximumDoubleClickTime)
+								{
+									last.ButtonsStates[i].Clicks=0;
+									last.ButtonsStates[i].Down=0;
+									last.ButtonsStates[i].MoveX=0;
+									last.ButtonsStates[i].MoveY=0;
+								}
+							}
+							#endregion
+
+							Softmouse3DButtons diffButtons=(buttons^last.Buttons);
+
+							// Order of click and double-click events in WindowsForms (want to provide the same here)
+							// 1. Down (Clicks: 1)
+							// 2. Click (Clicks: 1)
+							// 3. Up (Clicks: 1)
+							// ---- end of single click
+							// 4. Down (Clicks: 2)
+							// 5. DoubleClick (Clicks: 2)
+							// 6. Up (Clicks: 1)
+							// ? Should we support more Clicks? Is it enough to add up the clicks and raise DoubleClick again, or do we need an extra event?
+							// DoubleClick Time is form 1. down to next down. So we can reset the time last.ButtonState[?].Down time value on each button down.
+
+							if(diffButtons!=0) // any buttons changed?
+							{
+								// First the shift buttons down to support very fast button combination
+								if((buttons&Softmouse3DButtons.SL)!=0&&(last.Buttons&Softmouse3DButtons.SL)==0)
+								{
+								}
+
+								if((buttons&Softmouse3DButtons.SR)!=0&&(last.Buttons&Softmouse3DButtons.SR)==0)
+								{
+								}
+
+								// A and D Buttons
+								for(int b=0; b<8; b++)
+								{
+									Softmouse3DButtons button=buttonFilter[b];
+
+									if((diffButtons&button)!=0)
+									{
+										if((buttons&button)!=0) // pressed
+										{
+											////if(dEvt!=null) dEvt(this, new Softmouse3DButtonsEventArgs(inputHeader.header.hDevice, button, 
+
+											//if(last.ButtonsStates[b].Clicks>0) last.ButtonsStates[i].Clicks++; // Add another click
+											//else
+											//{
+											//	last.ButtonsStates[i].Clicks=1;
+											//	last.ButtonsStates[i].Down=ticks;
+											//	last.ButtonsStates[i].MoveX=0;
+											//	last.ButtonsStates[i].MoveY=0;
+											//}
+										}
+										else // released
+										{
+
+										}
+									}
+								}
+
+								// Last, but not least, the shift buttons releases
+								if((buttons&Softmouse3DButtons.SL)==0&&(last.Buttons&Softmouse3DButtons.SL)!=0)
+								{
+								}
+
+								if((buttons&Softmouse3DButtons.SR)==0&&(last.Buttons&Softmouse3DButtons.SR)!=0)
+								{
+								}
+
+								// save new last buttons
+								last.Buttons=buttons;
+							}
 						}
-
 					}
 				}
 				finally
